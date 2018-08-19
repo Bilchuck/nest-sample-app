5ecab414367c12293158401dcbfeb58b349120c9
Bilchuck <anton.bilchuk@stylelounge.com>
2018-08-19 13:22:39 +0300

    feature: init app. cats controller :3

---
+++ src/cats.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Req, Body } from "@nestjs/common";
+
+@Controller('cats')
+export class CatsController {
+  cats = [1,2,3,4]
+  constructor() {
+
+  }
+
+  @Get()
+  getCats() {
+    return this.cats
+  }
+
+  @Post()
+  async addCat(@Body('cat') newCat) {
+    await (new Promise(resolve => {
+      setTimeout(_ => {
+        this.cats.push(newCat)
+        resolve()
+      }, 1000)
+    }))
+    return true
+  }
+}
